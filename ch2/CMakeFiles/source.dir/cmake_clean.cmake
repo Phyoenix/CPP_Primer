69e249d01be03ef631a2a1a0db1b27e9842396f2
Flarenix <tangshenghao2@gmail.com>
2024-08-06 13:17:27 +0800

    nihao

---
+++ ch2/CMakeFiles/source.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source.dir/src/source.cpp.o"
+  "CMakeFiles/source.dir/src/source.cpp.o.d"
+  "bin/source"
+  "bin/source.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
